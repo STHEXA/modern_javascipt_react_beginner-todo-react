e38046461f7b5d1d550165e44566dce598692d05
STHEXA <ti.syu.s@gmail.com>
2024-02-25 23:38:58 +0900

    propとstateの学習分

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
+import { useState } from 'react';
+import { ColorfulMessage } from './components/ColorfulMessage';
+
 export const App = () => {
-  const onClickButton = () => alert();
-  const contentStyle = {
-    color: 'blue',
-    fontSize: '18px',
-    margin: 100,
+  const [num, setNum] = useState(0);
+  const onClickCountUp = () => {
+    setNum((prev) => prev + 1);
+    setNum((prev) => prev + 1);
   };
   return (
     <div>
       <h1 style={{ color: 'red' }}>こんにちは！</h1>
-      <p style={contentStyle}>お元気ですか？</p>
-      <button onClick={onClickButton}>ボタン</button>
+      <ColorfulMessage color="blue">お元気ですか?</ColorfulMessage>
+      <ColorfulMessage color="green">元気です！</ColorfulMessage>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <p>{num}</p>
     </div>
   );
 };
